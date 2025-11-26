0828dc5df97b4136620417637ea196a3ff06273d
irfanzmohammed <irfanmuhammed877@gmail.com>
2025-11-26 12:43:41 +0530

    about

---
+++ about.js
@@ -0,0 +1,11 @@
+// about.js - Version A (your branch)
+
+function getAboutContent() {
+  return `
+    <h1>About Us</h1>
+    <p>Welcome to our website! We are working hard to improve user experience.</p>
+    <p>This content was written by Irfan on his branch.</p>
+  `;
+}
+
+export default getAboutContent;
