5c28973f18faa827a2eed8cf251dda6776242011
irfanzmohammed <135992771+irfanzmohammed@users.noreply.github.com>
2025-11-26 12:39:01 +0530

    Add about.js with company information

---
+++ about.js
@@ -0,0 +1,11 @@
+// about.js - Version B (main branch)
+
+function getAboutContent() {
+  return `
+    <h1>About Our Company</h1>
+    <p>We are committed to delivering high-quality education and upskilling services.</p>
+    <p>This text was added by another developer on the main branch.</p>
+  `;
+}
+
+export default getAboutContent;
